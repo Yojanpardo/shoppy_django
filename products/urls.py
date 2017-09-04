6c70e50d23a72cf84e3ed3ff28526c89d46835b4
Yojan Pardo <yojan.pardo@gmail.com>
2017-09-03 23:06:34 -0500

    creo los templates para productos detallados y todos los productos, dejo un archivo limpio para el home

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='hello')
+	url(r'^$', views.index, name='hello'),
+	url(r'^products/', views.all_products, name='all_products'),
+	url(r'^product/(?P<pk>[0-9]+)/$',views.product_detail, name='product_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader #importa el cargador de plntilla de django
 from django.http import HttpResponse
 from .models import Product
@@ -7,10 +7,29 @@ from .models import Product
 # Create your views here.
 
 def index(request):
-	product = Product.objects.order_by('id')
+	title = 'Shoppy django'
 	template = loader.get_template('home.html')
 	context = {
+		'title': title
+	}
+	return HttpResponse(template.render(context, request))
+
+def all_products(request):
+	title = 'Productos'
+	product = Product.objects.order_by('id')
+	template = loader.get_template('productos/products.html')
+	context = {
+		'title': title,
 		'product': product
 	}
 	return HttpResponse(template.render(context, request))
 	#return render(request, 'index.html') carga una pantilla.
+
+
+def product_detail(request, pk):
+	product = get_object_or_404(Product, pk=pk)
+	template = loader.get_template('productos/product_detail.html')
+	context = {
+		'product': product
+	}
+	return HttpResponse(template.render(context, request))
