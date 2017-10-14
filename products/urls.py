7e39c5cd5ebe4f7777413e92a409204083e650e3
Yojan Pardo <yojan.pardo@gmail.com>
2017-10-13 21:06:19 -0500

    agrego forms.py y la template new_product.html

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='hello'),
 	url(r'^products/', views.all_products, name='all_products'),
-	url(r'^product/(?P<pk>[0-9]+)/$',views.product_detail, name='product_detail')
+	url(r'^product/(?P<pk>[0-9]+)/$',views.product_detail, name='product_detail'),
+	url(r'^new_product/', views.new_product, name='new_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader #importa el cargador de plntilla de django
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -31,5 +32,20 @@ def product_detail(request, pk):
 	template = loader.get_template('productos/product_detail.html')
 	context = {
 		'product': product
+	}
+	return HttpResponse(template.render(context, request))
+
+def new_product(request):
+	if request.method == 'POST':
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			product = form.save()
+			product.save()
+			return HttpResponseRedirect('/products')
+	else:
+		form = ProductForm()
+	template = loader.get_template('productos/new_product.html')
+	context = {
+		'form':form
 	}
 	return HttpResponse(template.render(context, request))
