d7564d5907a72edf1076c508c1e37f210e94305f
Yojan Pardo <yojan.pardo@gmail.com>
2017-11-04 11:22:04 -0500

    configuro el archivo de settings.py para hacer coneccion con mi base de datos postgresql

--- shoppy/settings.py
+++ shoppy/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'shoppy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shoppy',
+        'USER': 'yojan',
+        'PASSWORD': 'Pollofrito1030?',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
