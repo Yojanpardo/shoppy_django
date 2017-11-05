e017f8e5065d349b27962e788ce83e0a89e3513e
Yojan Pardo <yojan.pardo@gmail.com>
2017-11-04 20:46:45 -0500

    agrego el template de login, ahora puedo hacer signup, login y logout a los usuarios

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+	url(r'^login/$', views.auth_login, name='auth_login'),
+	url(r'^logout$', auth_views.logout, {'next_page':'/'}, name='logout'),
 	url(r'^$', views.index, name='hello'),
 	url(r'^products/', views.ProductList.as_view(), name='all_products'),
 	url(r'^product/(?P<pk>[0-9]+)/$',views.ProductDetail.as_view(), name='product_detail'),
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader #importa el cargador de plntilla de django
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Product
@@ -19,25 +22,12 @@ def index(request):
 	}
 	return HttpResponse(template.render(context, request))
 
-#def all_products(request):
-#	title = 'Productos'
-#	product = Product.objects.order_by('id')
-#	template = loader.get_template('products/products.html')
-#	context = {
-#		'title': title,
-#		'product': product
-#	}
-#		return HttpResponse(template.render(context, request))
-	#return render(request, 'index.html') carga una pantilla.
 
+class ProductList(ListView):
+	model = Product
 
-def product_detail(request, pk):
-	product = get_object_or_404(Product, pk=pk)
-	template = loader.get_template('products/product_detail.html')
-	context = {
-		'product': product
-	}
-	return HttpResponse(template.render(context, request))
+class ProductDetail(DetailView):
+	model = Product
 
 def new_product(request):
 	if request.method == 'POST':
@@ -54,8 +44,41 @@ def new_product(request):
 	}
 	return HttpResponse(template.render(context, request))
 
-class ProductList(ListView):
-	model = Product
+def  auth_login(request):
+	if request.method == 'POST':
+		action = request.POST.get('action', None)
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+	
+		if action == 'signup':
+			user=User.objects.create_user(username=username,
+				                          password=password)
+			user.save()
+		elif action=='login':
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			return redirect('/')
+		
+	context = {}
+	return render(request, 'login/login.html',context)
 
-class ProductDetail(DetailView):
-	model = Product
+
+#def all_products(request):
+#	title = 'Productos'
+#	product = Product.objects.order_by('id')
+#	template = loader.get_template('products/products.html')
+#	context = {
+#		'title': title,
+#		'product': product
+#	}
+#		return HttpResponse(template.render(context, request))
+	#return render(request, 'index.html') carga una pantilla.
+
+
+#def product_detail(request, pk):
+#	product = get_object_or_404(Product, pk=pk)
+#	template = loader.get_template('products/product_detail.html')
+#	context = {
+#		'product': product
+#	}
+#	return HttpResponse(template.render(context, request))
